be8802e34208a25b323e39f290d1c4d10a1ab6c3
jojohahana <yohana.chriezt@gmail.com>
2022-08-26 14:02:30 +0700

    test

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout";
+
+export default function FirstPost() {
+    return (
+      <>
+        <h1>First Post</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesettingindustry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+        </p>
+      </>
+    );
+  }
---
+++ pages/posts/second-post.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout";
+
+export default function SecondPost() {
+  return (
+    <Layout>
+      <h1>Second Post</h1>
+      <p>
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old
+      </p>
+    </Layout>
+  );
+}
